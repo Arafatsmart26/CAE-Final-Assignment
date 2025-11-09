34ae9e8b3c839cf3a13fa37bb09b5dd38c28a315
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2025-11-09 14:16:29 +0100

    Base for branch

--- RISC_V_SIMULATOR/Reader.py
+++ RISC_V_SIMULATOR/Reader.py
@@ -1,15 +1,17 @@
-size = 4
-instructions = []
-scale = 16
-num_of_bits = 32
 
-with open('RISC_V_SIMULATOR/tests/task1/addlarge.bin', 'rb') as f:
-    while True:
-        chunk = f.read(size)
-        if not chunk:
-            break
-        instructions.append(bin(int(chunk.hex(),scale))[2:].zfill(num_of_bits))
+def Reader():
+    size = 4
+    instructions = []
+    scale = 16
+    num_of_bits = 32
 
-for i in instructions:
+    with open('RISC_V_SIMULATOR/tests/task1/addlarge.bin', 'rb') as f:
+        while True:
+            chunk = f.read(size)
+            if not chunk:
+                break
+            instructions.append(bin(int(chunk.hex(),scale))[2:].zfill(num_of_bits))
+    return instructions
+print(len(Reader()))
+for i in Reader():
     print(i)
-print(len(instructions))
