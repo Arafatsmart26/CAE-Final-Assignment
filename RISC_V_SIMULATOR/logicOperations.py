105ffb5d988e329f0b57b06eada9f4295a8241f4
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2025-11-10 17:16:22 +0100

    Logic

--- RISC_V_SIMULATOR/logicOperations.py
+++ RISC_V_SIMULATOR/logicOperations.py
@@ -6,11 +6,61 @@ scale=2
 
 #LOAD AND STORE
 def LB(instruction, registers, memory):
+    memoryCheck = 0
+    size = 8
+
     rs1 = binToInt(registers[int(instruction[12:17],scale)].getContents(),2)
     rd = registers[int(instruction[20:25],scale)]
-    imm = binToInt(instruction[0:12],2)
+    imm = binToInt(instruction[0:12],scale)
 
+    address = (hex(rs1+imm))
+    for i in memory:
+        if address == i.getMemoryAddress():
+            rd = i.getMemoryContent()[0:size]
+            memoryCheck = 1
+            break
+    if memoryCheck == 0:
+        rd = 0x00
+    
+    registers[int(instruction[20:25],scale)] = rd 
 
+def LH(instruction, registers, memory):
+    memoryCheck = 0
+    size = 16
+
+    rs1 = binToInt(registers[int(instruction[12:17],scale)].getContents(),2)
+    rd = registers[int(instruction[20:25],scale)]
+    imm = binToInt(instruction[0:12],scale)
+
+    address = (hex(rs1+imm))
+    for i in memory:
+        if address == i.getMemoryAddress():
+            rd = i.getMemoryContent()[0:size]
+            memoryCheck = 1
+            break
+    if memoryCheck == 0:
+        rd = 0x0000
+    
+    registers[int(instruction[20:25],scale)] = rd 
+
+def LW(instruction, registers, memory):
+    memoryCheck = 0
+    size = 32
+
+    rs1 = binToInt(registers[int(instruction[12:17],scale)].getContents(),2)
+    rd = registers[int(instruction[20:25],scale)]
+    imm = binToInt(instruction[0:12],scale)
+
+    address = (hex(rs1+imm))
+    for i in memory:
+        if address == i.getMemoryAddress():
+            rd = i.getMemoryContent()[0:size]
+            memoryCheck = 1
+            break
+    if memoryCheck == 0:
+        rd = 0x00000000
+    
+    registers[int(instruction[20:25],scale)] = rd 
 #LOAD IMMEADIATE
 def LUI(instruction, registers):
     registers[int(instruction[20:25],scale)].setContents(bin(int(instruction[0:20],2) << 12))
@@ -21,7 +71,7 @@ def ADD(instruction, registers):
     rs1 = registers[int(instruction[12:17],scale)]
     rs2 = registers[int(instruction[7:12],scale)]
 
-    rd=bin(int(rs1.getContents(),scale)+int(rs2.getContents(),scale))[2:]
+    rd=bin(binToInt(rs1.getContents(),scale)+binToInt(rs2.getContents(),scale))[2:]
     registers[int(instruction[20:25],scale)].setContents(rd)
 
 
