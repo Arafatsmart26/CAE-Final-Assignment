69b95d09fbfee61b922d66d4a08e627a38a619d6
SimonDamsgaard <Simonpdamsgaard@users.noreply.github.com>
2025-11-24 16:48:12 +0100

    Demo changes

--- RISC_V_SIMULATOR/Registers.py
+++ RISC_V_SIMULATOR/Registers.py
@@ -12,7 +12,5 @@ def Initialize():
     registers=[]
     for i in range(0,32):
         registers.append(Register(int(0)))
-    registers[2].setContents(0x7ffffff0) # set init value for the stack pointer, 'sp'
-    registers[3].setContents(0x10000000) # set init value for the global pointer, 'gp'
     return registers
 
